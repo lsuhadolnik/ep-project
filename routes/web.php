0f8abf6a22c39a696d539957be85aa5379eaf7b7
klogar <kl2164@student.uni-lj.si>
2018-12-20 10:52:10 +0100

    view orders

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,11 @@ Route::get('/order', function() {
     return view('order');
 });
 
+/*tukaj mora priti se id od uporabnika / prodajalca?*/
+Route::get('/orders', function() {
+    return view('orders');
+});
+
 /*tukaj mora priti se id od userja?*/
 Route::get('/cart', function() {
     return view('shopping-cart');
