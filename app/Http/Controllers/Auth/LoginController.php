2d659810f92535d87c36f72fa97a453b056f1520
Lovro Suhadolnik <jest@lovro.rocks>
2018-12-21 22:23:19 +0100

    Uporabnik se ne more prijaviti, če je onemogočen.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -35,6 +40,20 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function attemptLogin(Request $request)
+    {
+        return Auth::attempt(
+            $this->credentials($request) + ["status" => 'active']
+        );
+    }
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        throw ValidationException::withMessages([
+            "email" => ["Geslo ni pravilno, ali pa je uporabnik blokiran."],
+        ]);
     }
 
 }
