0d975a4b4604f9bf24f7afdfb993048c8b13596d
klogar <kl2164@student.uni-lj.si>
2018-12-23 20:35:22 +0100

    added link on bestseller

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,20 +5,22 @@
     <div class="row justify-content-center">
         <div class="col-lg-12">
             <div class="title">
-                <h4> Najbolje prodajano </h4>
+                <h4> Najbolje ocenjeno </h4>
             </div>
             <div class="container-fluid">
                 <div class="row justify-content-around">
                     @foreach ($topProducts as $product)
+                    <a href="{{ route('showproduct', ['id' => $product->id]) }}" class="product-link">
                         <div class="bestseller ">
                             <div>
                                 <img src=" {{ asset('svg/no-image.png') }}" alt="slika izdelka" class="rounded">
                             </div>
-                            <p>{{$product->name}}
+                            <p style="padding-bottom:10px;">{{$product->name}}
                                 <br>
                                 {{$product->price}}$
                             </p>
                         </div>
+                    </a>
                     @endforeach
                 </div>
             </div>
