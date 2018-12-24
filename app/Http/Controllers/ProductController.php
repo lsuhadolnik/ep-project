d28a9c926324a2718521ad1bf4c6cc8742ce9742
klogar <kl2164@student.uni-lj.si>
2018-12-24 09:25:47 +0100

    added auth to product, changed redirect of register

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,6 +11,12 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except('show');
+    }
+
     public function show($product_id)
     {
         $quantity = 0;
