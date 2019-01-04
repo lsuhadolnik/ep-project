2af40fcf0309553d95a81b91f20dfc9db3ec0456
klogar <kl2164@student.uni-lj.si>
2019-01-04 11:46:28 +0100

    route group middleware

--- routes/web.php
+++ routes/web.php
@@ -11,50 +11,57 @@
 |
 */
 
+
 Route::get('/', 'MainController@show');
 Route::get('/search', 'MainController@search');
 
-Route::get('/order/{id}', 'OrderController@showOrder');
-Route::post('/order/{id}', 'OrderController@addOrder');
+Route::group(['middleware' => ['https', 'auth']], function() {
+    Route::get('/order/{id}', 'OrderController@showOrder');
+    Route::post('/order/{id}', 'OrderController@addOrder');
 
-Route::get('/orders', 'OrderController@show')->name('showorders');
-Route::get('/orders/{status}', 'OrderController@showByStatus')->name('filterorders');
+    Route::get('/orders', 'OrderController@show')->name('showorders');
+    Route::get('/orders/{status}', 'OrderController@showByStatus')->name('filterorders');
 
-Route::post('/invoice', 'OrderController@invoice');
-Route::get('/cart', 'ShoppingCartController@show')->name('showcart');
-Route::post('/cart', 'ShoppingCartController@add')->name('cart');
-Route::delete('/cart/{id}', 'ShoppingCartController@delete')->name('delcart');
+    Route::post('/invoice', 'OrderController@invoice');
+    Route::get('/cart', 'ShoppingCartController@show')->name('showcart');
+    Route::post('/cart', 'ShoppingCartController@add')->name('cart');
+    Route::delete('/cart/{id}', 'ShoppingCartController@delete')->name('delcart');
 
-Route::get('/profile', 'ProfileController@show')->name('showprofile');
-Route::post('/profile', 'ProfileController@update')->name('profile');
-Route::post('/resetPassword', 'ProfileController@resetPassword')->name('reset');
+    Route::get('/profile', 'ProfileController@show')->name('showprofile');
+    Route::post('/profile', 'ProfileController@update')->name('profile');
+    Route::post('/resetPassword', 'ProfileController@resetPassword')->name('reset');
 
 
 
-Route::get('/product/{id}', 'ProductController@show')->name('showproduct');
-Route::post('/product/{id}/rating', 'ProductController@rate')->name('rate');
+    Route::get('/product/{id}', 'ProductController@show')->name('showproduct');
+    Route::post('/product/{id}/rating', 'ProductController@rate')->name('rate');
+    Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout')->name('logout');
+});
 
-Route::get('/secure/management', 'ManagementController@show')->name('management');
+Route::group(['middleware' => ['https', 'auth', 'cert']], function() {
 
-Route::get('/secure/products', 'ManagementController@showProducts');
-Route::get('/secure/addProduct', 'ManagementController@showAddProduct');
-Route::post('/secure/addProduct', 'ManagementController@addProduct');
-Route::get('/secure/updateProduct/{id}', 'ManagementController@showUpdateProduct');
-Route::post('/secure/updateProduct/{id}', 'ManagementController@updateProduct');
-Route::delete('secure/deleteProductImage/{product_id}/{image_id}', 'ManagementController@deleteImage');
-Route::post('/secure/product/{id}/changeStatus', 'ManagementController@productChangeStatus');
 
-Route::get('/secure/order/{id}', 'ManagementController@showOrder');
-Route::get('/secure/orders/{status}', 'ManagementController@showByStatus')->name('ordersmanagement');
-Route::post('/secure/order/{id}/{status}', 'ManagementController@setStatus');
+    Route::get('/secure/management', 'ManagementController@show')->name('management');
 
-Route::get('/secure/users', 'ManagementController@showUsers');
-Route::post('/secure/user/{id}/changeStatus', 'ManagementController@userChangeStatus');
-Route::get('/secure/user/{id}', 'ManagementController@showUser');
-Route::post('/secure/user/{id}', 'ManagementController@updateUser');
-Route::post('/secure/changeRole/{id}', 'ManagementController@changeRole');
+    Route::get('/secure/products', 'ManagementController@showProducts');
+    Route::get('/secure/addProduct', 'ManagementController@showAddProduct');
+    Route::post('/secure/addProduct', 'ManagementController@addProduct');
+    Route::get('/secure/updateProduct/{id}', 'ManagementController@showUpdateProduct');
+    Route::post('/secure/updateProduct/{id}', 'ManagementController@updateProduct');
+    Route::delete('secure/deleteProductImage/{product_id}/{image_id}', 'ManagementController@deleteImage');
+    Route::post('/secure/product/{id}/changeStatus', 'ManagementController@productChangeStatus');
 
-Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout')->name('logout');
+    Route::get('/secure/order/{id}', 'ManagementController@showOrder');
+    Route::get('/secure/orders/{status}', 'ManagementController@showByStatus')->name('ordersmanagement');
+    Route::post('/secure/order/{id}/{status}', 'ManagementController@setStatus');
+
+    Route::get('/secure/users', 'ManagementController@showUsers');
+    Route::post('/secure/user/{id}/changeStatus', 'ManagementController@userChangeStatus');
+    Route::get('/secure/user/{id}', 'ManagementController@showUser');
+    Route::post('/secure/user/{id}', 'ManagementController@updateUser');
+    Route::post('/secure/changeRole/{id}', 'ManagementController@changeRole');
+});
+    
 
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
